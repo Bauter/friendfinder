2e1354d085303bd9a931e5c44046b0c7b7edb987
Bauter <bauter09@gmail.com>
2020-03-11 18:15:06 -0500

    fixed MIME error with CSS files not reading. fixed with line 17 in 'server.js'

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -14,6 +14,7 @@ const express = require("express")
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static('app/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
