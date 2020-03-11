1ae632529f7f775da38468f130cd8722693f81d4
Bauter <bauter09@gmail.com>
2020-03-10 19:54:39 -0500

    survey questions and dummy data set up

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -6,62 +6,62 @@ let friends = [
     {
         name: "Brian Quinn",
         photo: "https://www.imdb.com/name/nm1978079/mediaviewer/rm3868309248",
-        scores: []
+        scores: [4, 2, 3, 3, 5, 4, 2, 5, 2, 3]
     },
     {
         name: "Joe Gatto",
         photo: "https://www.imdb.com/name/nm2665746/mediaviewer/rm1278301184",
-        scores: []
+        scores: [5, 1, 3, 5, 2, 5, 4, 3, 2, 3]
     },
     {
         name: "James Murray",
         photo: "https://www.imdb.com/name/nm2098978/mediaviewer/rm2234602496",
-        scores: []
+        scores: [5, 5, 1, 5, 2, 3, 2, 3, 5, 5]
     },
     {
         name: "Sal Vulcano",
         photo: "https://www.imdb.com/name/nm1742600/mediaviewer/rm3985749760",
-        scores: []
+        scores: [3, 1, 4, 4, 1, 3, 1, 5, 3, 3]
     },
     {
         name: "Fluffy the keyboarding cat",
         photo: "https://images.pexels.com/photos/2071881/pexels-photo-2071881.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [1, 5, 5, 1, 5, 5, 1, 1, 1, 5]
     },
     {
         name: "Amy H",
         photo: "https://images.pexels.com/photos/732425/pexels-photo-732425.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-        scores: []
+        scores: [3, 2, 1, 4, 5, 5, 5, 5, 5, 1]
     },
     {
         name: "Bob W",
         photo: "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [1, 1, 5, 3, 1, 5, 5, 5, 2, 2]
     },
     {
         name: "Lily S",
         photo: "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [3, 1, 1, 3, 1, 3, 1, 5, 1, 5]
     },
     {
         name: "Highway Harry",
         photo: "https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [5, 1, 3, 1, 3, 5, 1, 5, 5, 1]
     },
     {
         name: "Ashley M",
         photo: "https://images.pexels.com/photos/2101841/pexels-photo-2101841.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [3, 3, 2, 4, 3, 4, 4, 4, 3, 3]
     },
     {
         name: "David J",
         photo: "https://images.pexels.com/photos/2743754/pexels-photo-2743754.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [3, 4, 4, 3, 3, 2, 1, 2, 3, 3]
     },
     {
         name: "Luis K",
         photo: "https://images.pexels.com/photos/2091327/pexels-photo-2091327.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        scores: []
+        scores: [1, 3, 1, 4, 1, 3, 2, 2, 1, 4]
     }
 ];
 
